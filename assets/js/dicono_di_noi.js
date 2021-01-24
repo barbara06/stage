30e556ca459c0903a9d5d09fde06af776d9ff378
barbara06 <67117994+barbara06@users.noreply.github.com>
2021-01-24 16:37:15 +0100

    24/01

--- assets/js/dicono_di_noi.js
+++ assets/js/dicono_di_noi.js
@@ -1,6 +1,6 @@
 /* Scheda dicono di noi */
 let recensioni = "";
-fetch('https://raw.githubusercontent.com/barbara06/stage/main/dicono_di_noi.json')
+fetch('https://raw.githubusercontent.com/barbara06/stage/main/recensioni.json')
     .then(
         function(response) {
             if (response.status !== 200) {
@@ -20,7 +20,7 @@ function creaElencoRecensioni(recensioni) {
     for (let i = 0; i < recensioni.length; i++) {
         let recensione = '<div class="recensione' + i + '">' +
                           '<h2 id="nome_recensione' + i + '">' + recensioni[i].nome + '</h2>' +
-                          '<p id="info_recensione' + i + '">' + recensioni[i].corso + '" | "' + recensioni[i].data + '</p>' +
+                          '<p id="info_recensione' + i + '">' + recensioni[i].corso + ' | ' + recensioni[i].data + '</p>' +
                           '<p id="recensione' + i + '">' + recensioni[i].recensione + '</p></div>';
 
         $("#elenco_recensioni").append(recensione);
