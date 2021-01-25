01a069f7e3721479d78ca9f61babe779dd97a7c9
barbara06 <67117994+barbara06@users.noreply.github.com>
2021-01-25 12:14:06 +0100

    25/01

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 /* Scheda corsi random */
-let corsi = "";
+let alternative = "";
 let array = [];
 fetch('https://raw.githubusercontent.com/barbara06/stage/main/corsi.json')
     .then(
@@ -12,52 +12,52 @@ fetch('https://raw.githubusercontent.com/barbara06/stage/main/corsi.json')
             // Examine the text in the response
             response.json().then(function(data) {
 
-                corsi = data.corsi;
+                alternative = data.corsi;
 
 
                 for (let i = 0; i < 3; i++) {
-                  let indice = Math.floor(Math.random() * corsi.length);
-                  array.push(corsi[indice]);
-                  corsi.splice(indice, 1);
+                  let indice = Math.floor(Math.random() * alternative.length);
+                  array.push(alternative[indice]);
+                  alternative.splice(indice, 1);
                 }
 
-                corsi = array;
-                creaListaRandom(corsi);
+                alternative = array;
+                creaListaRandom(alternative);
             })
 
                 
           });
     
-function creaListaRandom(corsi) {
+function creaListaRandom(alternative) {
 
-    if (corsi.length == 0) {
+    if (alternative.length == 0) {
         
         $("#lista_random").append('<div class="vuoto">Nessun corso trovato</div>');
 
         return false;
     }
 
-    for (let i = 0; i < corsi.length; i++) {
-        let corso = '<div class="anteprima_corso" onclick="infoCorso(' + i + ')">' +
-                        '<img id="img_corso' + i + '" class="img_corso" src="' + corsi[i].immagine + '"alt="Immagine corso">' +
+    for (let i = 0; i < alternative.length; i++) {
+        let alternativa = '<div class="anteprima_corso">' +
+                        '<img id="img_corso' + i + '" class="img_corso" src="' + alternative[i].immagine + '"alt="Immagine corso">' +
                         '<div class="info_corso">' +
-                            '<h2 class="titolo_corso">' + corsi[i].titolo + '</h2>' +
-                            '<p>' + corsi[i].obiettivi + '</p>' +
+                            '<h2 class="titolo_corso">' + alternative[i].titolo + '</h2>' +
+                            '<p>' + alternative[i].obiettivi + '</p>' +
                         '</div>' +
                             '<ul class="specifiche_corso">' +
-                                '<li>' + corsi[i].quota + '</li>' +
-                                '<li><i class="fa fa-user" aria-hidden="true"></i> ' + corsi[i].destinatari + '</li>' +
-                                '<li><i class="fa fa-line-chart" aria-hidden="true"></i> ' + corsi[i].livello + '</li>';
+                                '<li>' + alternative[i].quota + '</li>' +
+                                '<li><i class="fa fa-user" aria-hidden="true"></i> ' + alternative[i].destinatari + '</li>' +
+                                '<li><i class="fa fa-line-chart" aria-hidden="true"></i> ' + alternative[i].livello + '</li>';
 
-        if (corsi[i].modalita == "In presenza") {
-            corso = corso + '<li><i class="fa fa-users" aria-hidden="true"></i> ' + corsi[i].modalita + '</li></ul>';
+        if (alternative[i].modalita == "In presenza") {
+            alternativa = alternativa + '<li><i class="fa fa-users" aria-hidden="true"></i> ' + alternative[i].modalita + '</li></ul>';
         }
         else {
-            corso = corso + '<li><i class="fa fa-desktop" aria-hidden="true"></i> ' + corsi[i].modalita + '</li></ul>';
+            alternativa = alternativa + '<li><i class="fa fa-desktop" aria-hidden="true"></i> ' + alternative[i].modalita + '</li></ul>';
         }
-        corso = corso + '</div>';
+        alternativa = alternativa + '</div>';
         
-        $("#lista_random").append(corso);
+        $("#lista_random").append(alternativa);
     }   
 }
 
